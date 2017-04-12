78eecfa35e784107b9eab8ef86e96dad8276af35
gagannaidu <gagan.globin76@gmail.com>
2017-04-12 15:51:31 +0530

    Basic Python program with strings

---
+++ python-strings.py
@@ -0,0 +1,18 @@
+a = 'first line with single quot\'e'
+b = "second \\line with double quote"
+c = """ third \\line with triple quote"""
+d = 12
+
+print (a)
+print (b)
+print (c)
+
+print (len(a))
+
+print (a + b)
+
+print (a + str(d))
+
+print ((len(a) + len(b))*2)
+
+
