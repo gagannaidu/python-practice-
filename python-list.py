6434fe9ab57f4033a6f33865ec67b51f9f15218f
gagannaidu <gagan.globin76@gmail.com>
2017-04-12 16:53:55 +0530

    basic python program sing lists

---
+++ python-list.py
@@ -0,0 +1,17 @@
+names = ['gagan','karan','charan']
+
+ages = [25,26,23]
+
+print (ages)
+
+print (names,ages)
+
+print(len(names))
+
+print (max(names))
+
+print (max(ages))
+
+names.extend(ages)
+
+print (names)
