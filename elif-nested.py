81b161245c3cee8e02007f118d6e57267cd730b7
gagannaidu <gagan.globin76@gmail.com>
2017-04-18 12:11:33 +0530

    if else programming in python

---
+++ elif-nested.py
@@ -0,0 +1,22 @@
+name = input("Name is:")
+if name=="john":
+	print("Entered name is:",name)
+elif name=="mani":
+	print("name is:",name)
+elif name=="player":
+	print("name is:",name)
+else:
+	print("name is invalid")
+
+
+name = input("Enter  name:")
+carName = input("Enter car name:")
+if name=="car":
+	if carName=="Audi":
+		print ("Car name is:",carName)
+	else:
+		print ("car name is not Audi")
+	print("audi is:",name)
+else:
+	print("not a valid car")
+
---
+++ ifelse.py
@@ -0,0 +1,7 @@
+n = int(input("Enter Number"))
+if n<0:
+	print ("Absolute number of",n,"is",-n)
+else:
+	print ("Absolute number of",n,"is",n)
+sumoftwo=50+20
+print(sumoftwo)
