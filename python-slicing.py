44cb867af617bf6266bf65e9fc5cdcee616fb762
gagannaidu <gagan.globin76@gmail.com>
2017-04-18 10:12:44 +0530

    Sequence concept in python programming

---
+++ python-slicing.py
@@ -0,0 +1,9 @@
+mylist = [0,1,2,3,4,5,6,7,8,9]
+print (mylist)
+print (mylist[4:9])
+print (mylist[:8])
+print (mylist[2:10])
+print (mylist[2:])
+print (mylist[0:10:3])
+print (mylist[::5])
+print (mylist[:-5])
